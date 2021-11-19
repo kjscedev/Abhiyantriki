5aca4870e983a84e6caeb72d6659b1875ed17e42
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-11-19 09:14:29 +0530

    make live page responsive

--- src/pages/LiveStream.tsx
+++ src/pages/LiveStream.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface Props {}
 
 const LiveStream = (props: Props) => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+
+  const updateDimensions = () => {
+    setWidth(window.innerWidth);
+  };
+
+  let useHeight = 100;
+  if (width < 800 && width > 600) {
+    useHeight = 80;
+  } else if (width < 600) {
+    useHeight = 60;
+  }
+
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: useHeight.toString() + "vh" }}
+    >
       <iframe
         src="https://www.youtube.com/embed/GtIneRGZFLA"
-        height="80%"
-        width="80%"
+        height={useHeight === 60 ? "" : "80%"}
+        width={useHeight === 60 ? "" : "80%"}
         frameBorder="0"
         allow="autoplay; encrypted-media"
         allowFullScreen
