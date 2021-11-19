08ba8394a892e007663d223634b4bed78a02e73b
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-11-19 08:14:45 +0530

    reposition live button

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -16,7 +16,7 @@ const HomePage = () => {
       />
       <div
         className="position-absolute"
-        style={{ zIndex: 100, marginTop: "50vh" }}
+        style={{ zIndex: 1000, marginTop: "80vh" }}
       >
         <SpaceButton href={process.env.PUBLIC_URL + "/live"} text="View Live" />
       </div>
